8189dd7767ca3fd4fe161241f29dc45cf3686b2e
Nathan <nathancleon@gmail.com>
2018-06-26 13:59:52 -0400

    updated css styles and infowindow functionality

--- main.js
+++ main.js
@@ -1,21 +1,6 @@
-//project set up:
-//  store zipcode entered as lat long using google geocoding
-//  replace lat long in google maps initMap function to the geocode
-//  complete api request to foursquare for dog parks data
-//  iterate through dog parks data and obtain geocodes for each location
-//  obtain description and photo if available
-//  display parks on google maps as markers with info windows
-//  display photo, description, and address in info window when clicked
-//  see about styling markers and info windows
-//  once js app functions as intended, look into performance optimizations
-//  then style the app
-
-//  REMEMBER: Sidebar will be necessary to display park info and address info next to map
-//      sidebar will be moved to below the map in smaller screens
-//      play around with map size/zoom setting so that it works appropriately for all sizes
-
 let map;
 let latlng;
+let markerInfoMap = {};
 const FOURSQUARE_ENDPOINT = 'https://api.foursquare.com/v2/venues/search?ll=';
 const FOURSQUARE_ID = "IB2LJDSANFJOZ2OL2DCJUBWMUXINDXOBB1GYUBJ12IXG1V0K";
 const FOURSQUARE_CLIENT_SECRET = "QIXEV4V30G2ONJPEBIYUUWKQXZSQW3PRQ1V5AAZB1NG5QEY4";
@@ -74,29 +59,47 @@ function fourSquareGet() {
                 marker.setMap(map);
 
                 let infowindow = new google.maps.InfoWindow({
-                  content: venueHtml
+                  content: venueHtml,
+                  maxWidth: 250
                 });
 
                 marker.addListener('click', function() {
                   infowindow.open(map, marker);
                 });
+
+                google.maps.event.addListener(map, 'click', function() {
+                    infowindow.close();
+                });
+
+                markerInfoMap[venue.id] = [marker, infowindow];
+
             });
             $('#map').show(); 
+
             $('.js-foursquare-results').html(venuesHtml);
+            $('.js-foursquare-results').show();
+
+            $('.js-result, .js-result-title, .js-result-address').on('click', function(e){
+                e = e || window.event; 
+                e = e.target || e.srcElement;
+                markerInfoMap[e.id][1].open(map, markerInfoMap[e.id][0]);
+            });
+            
     });
 }
 
 function displayResult(venue) {
-    let venueHtml = `<div class="js-result"><h3 class="js-result-title">${venue.name}</h3><ul class="js-result-content">`;
+
+    let venueHtml = `<div class="js-result" id="${venue.id}"><h3 class="js-result-title" id="${venue.id}">${venue.name}</h3><ul class="js-result-content">`;
     
     if(venue.location.formattedAddress[0]) {
-        venueHtml += `<li class="js-address">${venue.location.formattedAddress[0]},&nbsp;</li>`;
+        venueHtml += `<li class="js-result-address" id="${venue.id}">${venue.location.formattedAddress[0]},&nbsp;</li>`;
     }
     if(venue.location.formattedAddress[1]) {
-        venueHtml += `<li class="js-address">${venue.location.formattedAddress[1]},&nbsp;</li>`;
+        venueHtml += `<li class="js-result-address" id="${venue.id}">${venue.location.formattedAddress[1]},&nbsp;</li>`;
     }
     if(venue.location.formattedAddress[2]) {
-        venueHtml += `<li class="js-address">${venue.location.formattedAddress[2]}</li>`;
+        venueHtml += `<li class="js-result-address" id="${venue.id}">${venue.location.formattedAddress[2]}</li>`;
     }
     venueHtml += `</div>`;
 
